e3aaa4ec1b0e13cd64f3a0c66b903ff41a002b69
ivylu888 <91304989+ivylu888@users.noreply.github.com>
2022-11-03 23:59:22 -0700

    Add files via upload

--- main.java
+++ main.java
@@ -4,19 +4,15 @@ import javax.swing.JPanel;
 public class main {
 
   public static void main(String[] args) {
-    // create JFrame entity
-    JFrame frame = new JFrame("Lab6");
-    // Setting the width and height of frame
-    frame.setSize(600, 300);
+    JFrame frame = new JFrame("Lab 7");
+    frame.setSize(2500, 800);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 
     JPanel panel = new JPanel();
     frame.add(panel);
-    graphInterface.putComponents(panel);
+    graphInterface.placeComponents(panel);
     frame.setVisible(true);
+
   }
 
-
 }
-
